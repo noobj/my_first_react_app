b1af315f47a16056159cb5668f49de8cb5bfad4a
noobj <jingfu99@gmail.com>
2022-04-01 16:22:49 +0800

    refactor: compress App component into ControlPanel and MainContent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
 import { Component } from 'react';
 import { format, endOfMonth, startOfMonth, startOfYear } from 'date-fns';
-import DatePickerInput from './DatePickerInput';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
 import { Category } from '../interfaces/Category.interface';
-import CategoryList from './CategoryList';
-
-ChartJS.register(ArcElement, Tooltip, Legend);
+import ControlPanel from './ControlPanel';
+import { MainContent } from './MainContent';
 
 type AppState = {
   start: string;
@@ -61,60 +57,15 @@ export default class App extends Component<unknown, AppState> {
     else this.setState({ end: dateStr }, cbAfterDateChanged);
   };
 
-  turnCategoriesToChartData = () => {
-    const categoryNames = this.state.categories.map(({ name }: { name: string }) => name);
-    const categoryPercentages = this.state.categories.map((v: Category) => v.percentage);
-    const categoryColors = this.state.categories.map((v: Category) => v.color);
-
-    return {
-      labels: categoryNames,
-      datasets: [
-        {
-          label: 'expense',
-          backgroundColor: categoryColors,
-          data: categoryPercentages
-        }
-      ]
-    };
-  };
-
   render() {
-    const chartData = this.turnCategoriesToChartData();
-    const categories = this.state.categories;
-    const categoryLists = categories.map((category) => {
-      return <CategoryList key={category._id} category={category} />;
-    });
-
     return (
       <>
-        <div className="border-b border-slate-900/10 dark:border-slate-300/10">
-          <div className="m-2 relative flex items-center">
-            <input
-              type="image"
-              src="./favicon-96x96.png"
-              onClick={() => window.location.reload()}
-              className="w-10 align-bottom mr-2"
-            />
-            <DatePickerInput
-              isStart={true}
-              value={this.state.start}
-              onChange={this.changeHandler}
-            />
-            <i className="bi bi-arrow-right text-xl ml-3 mr-3" />
-            <DatePickerInput isStart={false} value={this.state.end} onChange={this.changeHandler} />
-          </div>
-        </div>
-        <div className="m-2">
-          <h1 className="text-3xl font-bold">
-            {this.state.total === -1 ? <>loading...</> : <>Total: {this.state.total}</>}
-          </h1>
-          <div className="flex flex-col sm:flex-row">
-            <div className="flex-auto max-w-screen-sm">
-              <Pie data={chartData} />
-            </div>
-            <div className="flex-auto flex-col">{categoryLists}</div>
-          </div>
-        </div>
+        <ControlPanel
+          start={this.state.start}
+          end={this.state.end}
+          changeHandler={this.changeHandler}
+        />
+        <MainContent categories={this.state.categories} total={this.state.total} />
       </>
     );
   }
